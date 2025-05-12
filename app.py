bf06f333d086a20f05d80f20b3fe5b089f76e235
mrunali <mrunalipantawane2596@gmail.com>
2025-05-13 00:49:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+#app.py
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route("/")
+def hello():
+  return "Hello from FLASK!"
+
+if _name_ == "_main_":
+  app.run(host="0.0.0.0", port=5000)
